1be360c414533efae479724df51df76548015384
itsmeyaw <yudhistira.wibowo@itsmeyaw.id>
2026-09-15 21:33:02 +0200

    fix: export compatibility for bun v1.4.0

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,11 +1,1 @@
-export {
-  createBreakpointDefinition,
-  ResponsiveTuiConfigurationError,
-  type BreakpointAxis,
-  type BreakpointDefinition,
-  type BreakpointMatch,
-  type BreakpointPair,
-  type BreakpointScale,
-  type BreakpointScales,
-  type BreakpointViewport,
-} from "./breakpoints.js";
+export * from "./breakpoints.js";
